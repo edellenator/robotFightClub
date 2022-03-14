6982c96dd57cc24847e21935e5c80252683ddabd
Erik Dell <edellenator@gmail.com>
2022-03-13 20:54:27 -0700

    var defined fight function start

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,21 @@
-function fight () {
-    window.alert ("The fight has begin!");
+var playerName = window.prompt ("What is your robots name");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log (playerName, playerHealth, playerAttack);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function fight () {
+    window.alert ("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log (playerName + "attacked" + enemyName + "." + enemyName + "health is now" + enemyHealth);
+    playerHealth = playerHealth - enemyAttack
+    console.log (enemyName + "attacked" + playerName + "." + playerName + "health is now" + playerHealth)
 };
 
-// fight ();
+fight ();
 
-var playerName=window.prompt ("What is your robots name");
-console.log(playerName);
 
