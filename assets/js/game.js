10657525cf0f977a74627830f5c0fa489434f88c
Erik Dell <edellenator@gmail.com>
2022-03-15 21:58:44 -0700

    fix fight/skip interaction

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,18 +62,26 @@ var randomNumber = function (min, max) {
 
 var fight = function fight (enemy) {
     console.log (enemy)
-    while (enemy.health > 0 && playerInfo.health > 0) {
+    var fightOrSkip = function() {
         var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose");
-        if (promptFight === "skip" || promptFight === "SKIP") {
+        promptFight = promptFight.toLowerCase()
+        if (!promptFight) {
+            window.alert ("You need to select a valid option")
+            return fightOrSkip();
+        }
+        if (promptFight === "skip") {
             window.alert (playerInfo.name + "has chosen to skip the fight");
             var confirmSkip = window.confirm ("Are you sure you'd like to quit?");
             if (confirmSkip) {
                 window.alert (playerInfo.name + "has decided to leave the fight. Goodbye!");
                 playerInfo.money = Math.max (0, playerInfo.money - 10);
                 console.log ("player money", playerInfo.money);
-                break;
+                shop();
             }
         }
+    }
+    while (enemy.health > 0 && playerInfo.health > 0) {
+        fightOrSkip ();
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
         enemy.health = Math.max(0, enemy.health - damage);
         console.log (playerInfo.name + " attacked " + enemy.name + "." + enemy.name + " health is now " + enemy.health);
